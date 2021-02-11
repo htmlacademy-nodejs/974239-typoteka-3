866a89320066da37cf21ff2821dd416fbb02c1e2
Alex Grinevich <agrinevich@tsconsulting.com>
2021-02-11 12:40:52 +0300

    Добавлено чтение МОК данных из файла

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -8,57 +8,20 @@ const {ExitCode} = require(`../../constants`);
 const DEFAULT_COUNT = 1;
 const MAX_ELEMENTS_COUNT = 1000;
 
-const FILE_NAME = `mocks.json`;
+const OUTPUT_FILE_NAME = `mocks.json`;
+const CATEGORIES_FILE_NAME = `data/categories.txt`;
+const ANOUNCES_FILE_NAME = `data/sentences.txt`;
+const TITLES_FILE_NAME = `data/titles.txt`;
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
-
-const ANOUNCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
+const getStringsArrayFromFile = async (fileName) => {
+  try {
+    const content = await fs.readFile(fileName, `utf8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
 
 const now = new Date().valueOf();
 const threeMonthAgo = subtractMonth(now, 3);
@@ -81,9 +44,13 @@ module.exports = {
     }
     count = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
-    const content = JSON.stringify(generateOffers({count, titles: TITLES, anounces: ANOUNCES, categories: CATEGORIES}));
+    const titles = await getStringsArrayFromFile(TITLES_FILE_NAME);
+    const anounces = await getStringsArrayFromFile(ANOUNCES_FILE_NAME);
+    const categories = await getStringsArrayFromFile(CATEGORIES_FILE_NAME);
+
+    const content = JSON.stringify(generateOffers({count, titles, anounces, categories}));
     try {
-      await fs.writeFile(FILE_NAME, content);
+      await fs.writeFile(OUTPUT_FILE_NAME, content);
       console.log(chalk.green(`Operation success. File created.`));
       process.exit(ExitCode.SUCCESS);
     } catch (err) {
