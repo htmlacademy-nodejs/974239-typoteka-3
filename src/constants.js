6804bbc07f89208ab89c582144a90f78955274da
Alex Grinevich <agrinevich@tsconsulting.com>
2021-02-15 11:34:06 +0300

    module2-task3 Добавлен сценарий server

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,15 @@ const ExitCode = {
   FAIL: 1,
 };
 
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
+
 module.exports = {
   ExitCode,
+  HttpCode,
 };
