86e82a73b33fc183073332c0e75aac46915bfb91
Гриневич Александр Васильевич (4029681) <grinevich@dev.vtb.ru>
2021-04-13 08:59:36 +0300

    Добавлены шаблоны на Pug

--- src/express/index.js
+++ src/express/index.js
@@ -2,16 +2,24 @@
 
 const express = require(`express`);
 const chalk = require(`chalk`);
+const path = require(`path`);
 
 const myRouter = require(`./routes/my`);
 const articlesRouter = require(`./routes/articles`);
 const mainRouter = require(`./routes/main`);
 
 const PORT = 8080;
+const PUBLIC_DIR = `public`;
+const TEMPLATES_DIR = `templates`;
 const app = express();
 
 app.use(`/`, mainRouter);
 app.use(`/my`, myRouter);
 app.use(`/articles`, articlesRouter);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set(`views`, path.resolve(__dirname, TEMPLATES_DIR));
+app.set(`view engine`, `pug`);
 
 app.listen(PORT, () => console.log(chalk.green(`Сервер запущен на порту ${PORT}`)));
--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -3,7 +3,7 @@
 const {Router} = require(`express`);
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.send(`/my`));
-myRouter.get(`/comments`, (req, res) => res.send(`/my/comments`));
+myRouter.get(`/`, (req, res) => res.render(`pages/admin-publications`));
+myRouter.get(`/comments`, (req, res) => res.render(`pages/admin-comments`));
 
 module.exports = myRouter;
