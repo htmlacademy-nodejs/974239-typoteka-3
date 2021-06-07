260e16c591d15079b6e71d6ee6839ce27693ec61
Alex <45149010+aleksgrin@users.noreply.github.com>
2021-06-07 15:58:06 +0300

    тесты и логгер (#11)
    Co-authored-by: Гриневич Александр Васильевич (4029681) <grinevich@dev.vtb.ru>

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,9 +1,11 @@
 "use strict";
 
 const express = require(`express`);
-const chalk = require(`chalk`);
+const {getLogger} = require(`../lib/logger`);
 const {HttpCode} = require(`../../constants`);
 const apiRouter = require(`../api`);
+
+const logger = getLogger({name: `api`});
 
 const app = express();
 
@@ -14,13 +16,40 @@ app.use(express.json());
 app.use(`/api`, apiRouter);
 
 app.use((req, res) => {
-  res.status(HttpCode.NOT_FOUND).send(`${req.url} Not found`);
+  res.status(HttpCode.NOT_FOUND)
+    .send(`Not found`);
+  logger.error(`Route not found: ${req.url}`);
+});
+
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+});
+
+app.use((err, _req, _res, _next) => {
+  logger.error(`An error occurred on processing request: ${err.message}`);
 });
 
 module.exports = {
   name: `--server`,
   run(port) {
     port = Number.parseInt(port, 10) || DEFAULT_PORT;
-    app.listen(port, () => console.info(chalk.green(`Ожидаю соединений на ${port}`)));
+
+    try {
+      app.listen(port, (err) => {
+        if (err) {
+          return logger.error(`An error occurred on server creation: ${err.message}`);
+        }
+
+        return logger.info(`Listening to connections on ${port}`);
+      });
+
+    } catch (err) {
+      logger.error(`An error occurred: ${err.message}`);
+      process.exit(1);
+    }
   },
 };
